f2c73b37db9bf8f8d3ca714090aa0d2b85db5de0
Dean Blackketter <dean@blackketter.com>
2015-10-20 10:17:38 -0700

    Remove some old commented out code.  Make the date a little smaller

--- ClockApp.cpp
+++ ClockApp.cpp
@@ -20,7 +20,6 @@ void ClockApp::update(void) {
     screen.setFont(Arial_72);
 
     screen.setTextColor(screen.luminance(currentColor) < 128 ? screen.brighten(currentColor, 128) : currentColor, bgColor());
-    currentColor++;
     sprintf(textTime, "%d:%02d", clock.hourFormat12(), clock.minute());
     coord_t w = screen.measureTextH(textTime);
     coord_t h = screen.measureTextV(textTime);
@@ -43,39 +42,9 @@ void ClockApp::update(void) {
     char longDate[100];
     clock.longDate(longDate);
 
+    screen.setFont(Arial_14);
     screen.setCursor(screen.clipMidWidth()-screen.measureTextH(longDate)/2,
     screen.clipBottom()-((screen.clipBottom()-(screen.clipMidHeight() + h/2))/2  + screen.measureTextV(longDate)/2));
     screen.drawText(longDate);
-
+    currentColor++;
 }
-/*
-// Draws analog clock
-  int x = random(screen.clipWidth());
-  int y = random(screen.clipHeight());
-  long r = random(40);
-  time_t tt = now();
-  long h = hourFormat12(tt);
-  long m = minute(tt);
-  long s = second(tt);
-  long t = h*60+m;
-
-  // clip out the area where we'll draw the text
-  screen.setClipRect(0, 20, screen.clipWidth(), screen.clipHeight());
-
-  screen.fillCircle(x, y, r, currentColor);
-  screen.drawCircle(x, y, r, ~currentColor);
-  // hands are 80% the radius of the clock
-  r = (r * 8)/10;
-
-  screen.drawLine(x,y, x + r*((long)sin8((t*255)/720)-128)/128/2, y - r*((long)cos8((t*255)/720)-128)/128/2, ~currentColor);
-  screen.drawLine(x,y, x + r*((long)sin8((m*255)/60)-128)/128, y - r*((long)cos8((m*255)/60)-128)/128, ~currentColor);
-
-  screen.setClipRect();  // reset the clip rect
-  screen.setCursor(0,0);
-  screen.setTextColor(currentColor, screen.black);
-
-  if (h == 0) h=12;
-  screen.drawTextF("%2d:%02d:%02d", h, m, s);
-  currentColor++;
-*/
-

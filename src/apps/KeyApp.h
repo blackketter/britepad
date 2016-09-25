66fd79ee7dea7d4e01528b1b4f8d9c3173afa546
Dean Blackketter <dean@blackketter.com>
2016-09-25 10:27:35 -0700

    Using a patched version of the FT6206 library (without warnings). Also, fix warning in building KeyApp

--- src/apps/KeyApp.h
+++ src/apps/KeyApp.h
@@ -5,12 +5,14 @@
 #include "BritepadShared.h"
 #include "widgets/Icon.h"
 
+typedef int mediakey_t;
+
 class KeyApp : public BritepadApp {
 
   public:
     KeyApp(const char* name, const char* keys, color_t color, uint32_t pos) { nameStr = name; keyStr = keys; button_color = color; setLauncherPosition(pos);};
-    KeyApp(const char* name, uint8_t key, color_t color, uint32_t pos) { nameStr = name; mediaKey = key; button_color = color; setLauncherPosition(pos);}
-    KeyApp(icon_t i, uint8_t key, color_t color, uint32_t pos) { icon = i; mediaKey = key; button_color = color; setLauncherPosition(pos);}
+    KeyApp(const char* name, mediakey_t key, color_t color, uint32_t pos) { nameStr = name; mediaKey = key; button_color = color; setLauncherPosition(pos);}
+    KeyApp(icon_t i, mediakey_t key, color_t color, uint32_t pos) { icon = i; mediaKey = key; button_color = color; setLauncherPosition(pos);}
 
     void run();
     const char* name() { return nameStr; };
@@ -28,7 +30,7 @@ class KeyApp : public BritepadApp {
     const char* nameStr = nullptr;
 
     const char* keyStr = nullptr;
-    uint8_t  mediaKey = 0;
+    mediakey_t  mediaKey = 0;
     color_t button_color = 0;;
     virtual void draw();
 };

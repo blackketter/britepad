146ae6cba1f160e5bd16c4b82206f672e7015833
Dean Blackketter <dean@blackketter.com>
2018-02-14 14:57:38 -0800

    Added a Dot Calendar screensaver

---
+++ src/apps/DotCalApp.cpp
@@ -0,0 +1,47 @@
+#include "BritepadShared.h"
+#include "DotCalApp.h"
+
+DotCalApp theDotCalApp;
+
+void DotCalApp::begin(AppMode asMode) {
+
+  if (!dots) {
+    dots = new DotMatrix(screen.clipLeft(), screen.clipTop(), screen.clipWidth(), screen.clipHeight(), getDotsWide(), getDotsHigh());
+  }
+
+  Time t;
+  uint16_t y = clock.year();
+  t.setDateTime(y);
+  while (t.year() == y) {
+    dots->setDot(t.day()-1,t.month()-1, t.millis() < clock.millis() ? beforeColor : afterColor);
+    t.adjustSeconds(Time::secsPerDay);
+  }
+
+  ClockApp::begin(asMode);
+  dots->draw();
+};
+
+void DotCalApp::update() {
+
+  // make sure the last dot is drawn correctly
+  Time t(clock);
+  t.adjustSeconds(-Time::secsPerDay);
+
+  if (t.year() == clock.year()) {
+    dots->setDot(t.day()-1, t.month()-1, beforeColor);
+    dots->updateDot(t.day()-1, t.month()-1);
+  }
+
+  dots->setDot(clock.day()-1, clock.month()-1, clock.now()%2 ? beforeColor : afterColor);
+  dots->updateDot(clock.day()-1, clock.month()-1);
+}
+
+void DotCalApp::end() {
+   if (dots) {
+    delete dots;
+    dots = nullptr;
+  }
+  ClockApp::end();
+}
+
+
---
+++ src/apps/DotCalApp.h
@@ -0,0 +1,33 @@
+#ifndef _DotCalApp_
+#define _DotCalApp_
+
+#include "ClockApp.h"
+#include "BritepadShared.h"
+#include "widgets/DotMatrix.h"
+#include "Screen.h"
+
+class DotCalApp : public ClockApp {
+  public:
+    void begin(AppMode asMode);
+    void update();
+    void end();
+
+    const char* name() { return "Dot Cal"; };
+    appid_t id() { return ID; };
+    static constexpr appid_t ID = "dcal";
+    virtual bool displaysClock() { return false; };
+
+  protected:
+    virtual int getDotsWide() { return 31; }
+    virtual int getDotsHigh() { return 12; }
+    DotMatrix* dots = nullptr;
+
+  private:
+    const color_t beforeColor = screen.white;
+    const color_t afterColor = screen.grey;
+    const color_t blankColor = screen.black;
+};
+
+#endif
+
+

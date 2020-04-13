ae55b72d748a9c468c2dbc4a043242bc2efddad9
Dean Blackketter <dean@blackketter.com>
2020-04-13 10:38:11 -0700

    Add Jenny Holzer quotes app

--- src/apps/QuotesApp.cpp
+++ src/apps/QuotesApp.cpp
@@ -1,5 +1,6 @@
 #include "BritepadShared.h"
 #include "ObliqueStrategies.h"
+#include "JennyHolzer.h"
 #include "ScreensaverApp.h"
 
 class QuotesApp : public ScreensaverApp {
@@ -13,7 +14,7 @@ class QuotesApp : public ScreensaverApp {
         screen.setTextAlign((alignment_t)(ALIGN_VCENTER|ALIGN_HCENTER));
 
         String wrapped;
-        const char* strat = randomObliqueStrategy();
+        const char* strat = randomQuote();
         console.debugf("Quote: %s\n",strat);
         screen.softWrapText(wrapped, strat);
         screen.drawString(wrapped);
@@ -25,14 +26,29 @@ class QuotesApp : public ScreensaverApp {
     }
     void begin(AppMode asMode) override { lastDrawn = 0; }
 
-    const char* name() override { return "Quotes"; };
-    appid_t id() override { return ID; };
-    static constexpr appid_t ID = "quot";
-
   protected:
     millis_t lastDrawn = 0;
+    virtual const char* randomQuote() = 0;
 };
 
-QuotesApp theQuotesApp;
+class HolzerApp : public QuotesApp {
 
+    const char* name() override { return "Holzer"; };
+    appid_t id() override { return ID; };
+    static constexpr appid_t ID = "holz";
 
+  protected:
+    const char* randomQuote() override { return randomJennyHolzer(); }
+};
+HolzerApp theHolzerApp;
+
+class ObliqueApp : public QuotesApp {
+
+    const char* name() override { return "Oblique"; };
+    appid_t id() override { return ID; };
+    static constexpr appid_t ID = "obli";
+
+  protected:
+    const char* randomQuote() override { return randomObliqueStrategy(); }
+};
+ObliqueApp theObliqueApp;

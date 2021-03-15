344a3247f962b899f3a866900927ac986b6f8b61
Dean Blackketter <dean@blackketter.com>
2021-03-15 09:12:04 -0700

    Add a cognitive biases flashcard app (content from wikipedia)

--- src/apps/QuotesApp.cpp
+++ src/apps/QuotesApp.cpp
@@ -1,6 +1,7 @@
 #include "BritepadShared.h"
 #include "ObliqueStrategies.h"
 #include "JennyHolzer.h"
+#include "CognitiveBiases.h"
 #include "ScreensaverApp.h"
 
 class QuotesApp : public ScreensaverApp {
@@ -8,7 +9,7 @@ class QuotesApp : public ScreensaverApp {
     void run() override {
       if (lastDrawn == 0) {
         clearScreen();
-        screen.setFont(&Arial_20_Bold);
+        screen.setFont(font());
         screen.setTextColor(screen.white, bgColor());
         screen.setCursor(screen.clipMidWidth(), screen.clipMidHeight());
         screen.setTextAlign((alignment_t)(ALIGN_VCENTER|ALIGN_HCENTER));
@@ -25,10 +26,11 @@ class QuotesApp : public ScreensaverApp {
 
     }
     void begin() override { lastDrawn = 0; }
-
+    virtual const ILI9341_t3_font_t* font() { return &Arial_20_Bold; }
   protected:
     millis_t lastDrawn = 0;
     virtual const char* randomQuote() = 0;
+
 };
 
 class HolzerApp : public QuotesApp {
@@ -52,3 +54,15 @@ class ObliqueApp : public QuotesApp {
     const char* randomQuote() override { return randomObliqueStrategy(); }
 };
 ObliqueApp theObliqueApp;
+
+class CognitiveBiasesApp : public QuotesApp {
+
+    const char* name() override { return "Biases"; };
+    appid_t id() override { return ID; };
+    static constexpr appid_t ID = "bias";
+
+  protected:
+    const char* randomQuote() override { return randomCognitiveBias(); }
+     const ILI9341_t3_font_t* font() override { return &Arial_12_Bold; }
+};
+CognitiveBiasesApp theCognitiveBiasesApp;
